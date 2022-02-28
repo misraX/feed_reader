fc75ed1e8d791d60ae673304861d92cd4d52be02
misraX <maysragamal@gmail.com>
2022-02-28 02:52:23 +0200

    Fixing test_http

--- apps/feed_parser/tests/test_http.py
+++ apps/feed_parser/tests/test_http.py
@@ -5,7 +5,7 @@ from apps.feed.tests.factories import FeedFactory
 from apps.feed_parser.http import parse_feed
 
 
-class HTTPTestCase(TestCase):
+class HTTPTest(TestCase):
     def test_parse_feed(self):
         feed: list = FeedFactory.create_batch(1)
         parsed, feed_items = parse_feed(feed[0])
